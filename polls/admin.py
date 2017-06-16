39965c1b5c2fa12c9f035f03e2f90774e74220f3
Alejandro Agustin B <alejandro.a.es@ieee.org>
2017-06-16 12:01:14 +0200

    Tutorial 7

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 
-from polls.models import Question
+from polls.models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3  # Cannot be deleted
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']  # Filter sidebar
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.site_header = 'Polls administration'
+admin.site.site_title = 'Polls admin site'
